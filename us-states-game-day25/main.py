6270720f541d7290f2de616e7b2530ae1ed08673
Dylan <dm5messerly@gmail.com>
2025-02-20 16:56:08 -0600

    added a US state guessing game, learning pandas

---
+++ us-states-game-day25/main.py
@@ -0,0 +1,60 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+IMAGE = "blank_states_img.gif"
+CSV_FILE = "50_states.csv"
+
+screen.addshape(IMAGE)
+turtle.shape(IMAGE)
+
+# used to get coordinates of the states on the map
+# def get_mouse_click_coor(x, y):
+#     print(x,y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+state_data = pandas.read_csv(CSV_FILE)
+
+state_names = state_data.state.to_list()
+
+cursor = turtle.Turtle()
+cursor.hideturtle()
+cursor.penup()
+
+correct_guesses = []
+
+while len(correct_guesses) < 50:
+
+    answer_state = screen.textinput(title=f"{len(correct_guesses)}/50 States", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in state_names:
+            if state not in correct_guesses:
+                missing_states.append(state)
+
+            states_to_learn = pandas.DataFrame(missing_states)
+            states_to_learn.to_csv("states_to_learn.csv")
+
+        break
+
+    item = state_data[state_data.state == answer_state]
+
+    if len(item) != 0:
+        # print(item.state)
+        if answer_state not in correct_guesses:
+            coordinates = (item.x.item() , item.y.item())
+            cursor.goto(coordinates)
+            correct_guesses.append(answer_state)
+
+            cursor.write(item.state.item())
+        else:
+            print(f"{answer_state} already guessed")
+    else:
+        print("wrong")
+
+
+# screen.exitonclick()

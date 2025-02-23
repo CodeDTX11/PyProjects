733b0c6220b0dd655b9df51a37aed9c3b62ba6a0
Dylan <dm5messerly@gmail.com>
2025-02-22 22:22:53 -0600

    added improved password manager app with exception handling, search button and json data storage

--- impoved-password-manager-day30/main.py
+++ impoved-password-manager-day30/main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import random
 #pyperclip used for clipboard interaction
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def gen_password():
@@ -10,47 +11,20 @@ def gen_password():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    # print("Welcome to the PyPassword Generator!")
-    # nr_letters = int(input("How many letters would you like in your password?\n"))
-    # nr_symbols = int(input(f"How many symbols would you like?\n"))
-    # nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-    # nr_letters = random.randint(8, 10)
-    # nr_symbols = random.randint(2, 4)
-    # nr_numbers = random.randint(2, 4)
-
-    # Hard level
-    # for char in range(0, nr_letters):
-    #     password_list.append(random.choice(letters))
-
     char_list = [random.choice(letters) for _ in range(random.randint(8, 10))]
 
-    # for char in range(0, nr_symbols):
-    #     password_list.append(random.choice(symbols))
-
     num_list = [random.choice(numbers) for _ in range(random.randint(2, 4))]
-
-    # for char in range(0, nr_numbers):
-    #     password_list.append(random.choice(numbers))
 
     symb_list = [random.choice(symbols) for _ in range(random.randint(2, 4))]
 
     password_list = char_list + num_list + symb_list
 
-    # print(password_list)
     random.shuffle(password_list)
-    # print(password_list)
-
-    # password = ""
-    # for char in password_list:
-    #     password += char
 
     password = "".join(password_list)
     password_input.delete(0, END)
     password_input.insert(0, password)
     pyperclip.copy(password)
-
-    # print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -58,23 +32,58 @@ def save():
     email_uname = email_uname_input.get()
     password = password_input.get()
 
+    new_data = {
+        website: {
+            "email": email_uname,
+            "password": password
+        }
+    }
+
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Make sure input is not empty")
     else:
-
         is_ok = messagebox.askokcancel(title=website, message=f"Ok to save info?\n"
                                                       f"{email_uname}\n"
                                                       f"{password}")
         if is_ok:
-            with open(file="data.txt", mode="a") as data:
-                data.write(f"{website} | {email_uname} | {password}\n")
+            try:
+                with open(file="data.json", mode="r") as data_file:
+                    #reading old data
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open(file="data.json", mode="w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                #updatating data
+                data.update(new_data)
 
-            website_input.delete(0,END)
-            password_input.delete(0,END)
+                with open(file="data.json", mode="w") as data_file:
+                    #saving updated data
+                    json.dump(data, data_file, indent=4)
+            finally:
+                website_input.delete(0,END)
+                password_input.delete(0,END)
 
             # messagebox.showinfo("Info", "Save successful")
+# ---------------------------- SEARCH --------------------------------------- #
+def search():
+    website = website_input.get()
+    if len(website) > 0:
+        try:
+            with open(file="data.json", mode="r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Error", message="No data file found")
+        else:
+            try:
+                found_entry = data[website]
+            except KeyError:
+                messagebox.showinfo(title="Sorry", message=f'The website "{website}" was not found')
+            else:
+                messagebox.showinfo(title=f"{website}", message=f"Email: {found_entry["email"]}\n"
+                                                                f"Password: {found_entry["password"]}")
 
-# ---------------------------- UI SETUP -------------------------------
+# ---------------------------- UI SETUP ------------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
@@ -88,9 +97,12 @@ canvas.grid(row=0, column=1, sticky=EW)
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0, sticky=W)
 
-website_input = Entry()
-website_input.grid(row=1, column=1, columnspan=2, sticky=EW)
+website_input = Entry(width=32)
+website_input.grid(row=1, column=1, columnspan=2, sticky=W)
 website_input.focus()
+
+search_button = Button(text="Search", command=search, bg="lightblue")
+search_button.grid(row=1, column=2, stick=EW)
 
 email_uname_label = Label(text="Email/Username: ")
 email_uname_label.grid(row=2, column=0, sticky=W)
